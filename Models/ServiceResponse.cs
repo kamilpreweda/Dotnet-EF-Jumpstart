6a8020b63b26ea1d92a2b4cf3869df3bbcd3d174
Kamil Preweda <preweda.kamil@o2.pl>
2023-03-27 09:53:27 +0200

    Implemented ServiceResponse

---
+++ Models/ServiceResponse.cs
@@ -0,0 +1,9 @@
+namespace Dotnet_EF_Jumpstart.Models
+{
+    public class ServiceResponse<T>
+    {
+        public T? Data { get; set; }
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = string.Empty;
+    }
+}
