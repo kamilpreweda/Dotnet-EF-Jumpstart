57c9878967037944d32794d50887627465c6e9bd
Kamil Preweda <preweda.kamil@o2.pl>
2023-03-29 11:49:24 +0200

    Implemented User Registration

---
+++ Dtos/User/UserRegisterDto.cs
@@ -0,0 +1,8 @@
+namespace Dotnet_EF_Jumpstart.Dtos.User
+{
+    public class UserRegisterDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
