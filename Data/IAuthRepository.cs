2a68ec590cbd20a7b801bb5448317e5a866b1d6d
Kamil Preweda <preweda.kamil@o2.pl>
2023-03-29 11:29:24 +0200

    Created IAuthRepository interface

---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,9 @@
+namespace Dotnet_EF_Jumpstart.Data
+{
+    public interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
